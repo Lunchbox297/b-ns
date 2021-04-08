1b94b0e89110cb1f3f1a4ca86b2a5f805070de4a
Theo Claydon <TClaydon@stu.kerikerihigh.ac.nz>
2021-04-08 14:52:04 +1200

    ain

--- game.py
+++ game.py
@@ -1,2 +1,10 @@
-print('your nuts are small')
-print("some more changes")
+import arcade
+
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
+
+
+window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+arcade.run()
